91c9d7125e9508bce982ab552a58a7925084ddbd
Lunga Tsewu <tswlun002@cs.uct.ac.za>
2022-06-18 22:48:54 +0200

    Added  button to view Resume, github, email, linkedin and twitter

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ *Opens window to email to 1998tsewu@gmail.com
+ */
+function emailTo(){
+    document.location.href = "mailto:1998tsewu@gmail.com";
+};
+
+/***
+ * Opens github profile 
+ */
+function toGithub(){
+    document.location.href="https://github.com/tswlun002"
+}
+/***
+ * Open linkedin to my profile
+ */
+function toLinkedin(){
+    document.location.href="https://www.linkedin.com/in/lunga-tsewu-b22058181/"
+}
+/**
+ * opens twitter to my profile
+ */
+function toTwitter(){
+    document.location.href="https://twitter.com/MrLuTs"
+}
